a15391ec0362e6279a90b99055011517a7efbfbb
Sanjeev N <96974806+InfinityDeveLopER0@users.noreply.github.com>
2026-06-29 13:22:53 +0530

    fix

--- app/event/[slug]/page.tsx
+++ app/event/[slug]/page.tsx
@@ -5,7 +5,7 @@ const EventDetailsPage = async ({params}: {params: Promise<{slug: string}>}) => 
     const slug = params.then((p) => p.slug)
     return (
         <main>
-            <Suspense fallback= { <div> Loading...</div>} >
+            <Suspense fallback={<div> Loading...</div>}>
                 <EventDetails params={slug} />
             </Suspense>
         </main>
